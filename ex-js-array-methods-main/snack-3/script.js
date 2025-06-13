3139f1892a7ff5105e1a0ba95e9539a74f7b4b90
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-13 14:52:14 +0200

    snack-3 is done

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,30 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
-// Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1 con ciclo for
+const numbersIncrement = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    const numbersIncrement = numbers[i] + 1;
+    console.log(numbersIncrement);
+};
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1 con forEach
+numbers.forEach((number, i) => {
+    const numbersIncrement = numbers[i] + 1;
+    console.log(numbersIncrement);
+});
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1 con forEach con one liner
+numbers.forEach((number, i) => console.log(number + 1));
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1 con map
+const numberUp = numbers.map((number, i) => {
+    return numbers[i] + 1;
+});
+console.log(numberUp);
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1 con map con one liner
+const numberAdd = numbers.map((number, i) => numbers[i] + 1);
+console.log(numberAdd);
+
 // Risultato: [3, 9, 5, 8, 3, 88]
