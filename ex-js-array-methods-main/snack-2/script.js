c211ad813f31530bb0a642edc699511808893d10
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-13 14:37:13 +0200

    snack-2 is done

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -4,5 +4,15 @@ const people = [
   { name: 'Marco', age: 67 }
 ];
 
-// Stampa in console tutti i nomi
+// Stampa in console tutti i nomi con ciclo for
+for (let i = 0; i < people.length; i++) {
+  const person = people[i].name;
+  console.log(person);
+};
+// Stampa in console tutti i nomi con forEach
+people.forEach((person, i) => {
+  console.log(people[i].name);
+});
+// Stampa in console tutti i nomi con forEach con one liner
+people.forEach((person, i) => console.log(people[i].name));
 // Risultato: 'Paolo', 'Giulia', 'Marco'
