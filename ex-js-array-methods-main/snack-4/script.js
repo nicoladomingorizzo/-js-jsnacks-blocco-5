90c77945f21d4897c7523318da9957f940e739f8
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-13 15:03:58 +0200

    snack-4 is done

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -5,5 +5,26 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
-// Crea un nuovo array coi nomi di tutti gli autori
+// Crea un nuovo array coi nomi di tutti gli autori con ciclo for
+const authorsNames = [];
+for (let i = 0; i < posts.length; i++) {
+  const authorName = posts[i].author;
+  authorsNames.push(authorName);
+};
+console.log(authorsNames);
+
+// Crea un nuovo array coi nomi di tutti gli autori con forEach
+const authorsNamesFe = [];
+posts.forEach((author, i) => {
+  const authorName = posts[i].author;
+  authorsNamesFe.push(authorName);
+});
+console.log(authorsNamesFe);
+
+// Crea un nuovo array coi nomi di tutti gli autori con forEach one liner
+const authorsNamesFeOl = [];
+posts.forEach((author, i) => authorsNamesFeOl.push(posts[i].author));
+console.log(authorsNamesFeOl);
+
+
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
