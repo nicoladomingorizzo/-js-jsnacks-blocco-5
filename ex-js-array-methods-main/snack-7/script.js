64c19b5fe42ee57e4246fc696ce5e640ee827775
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-13 15:50:00 +0200

    snack-7 is done

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -19,5 +19,29 @@ const students = [
   },
 ];
 
-// Recupera i dati dello studente con id 2
+// Recupera i dati dello studente con id 2 con ciclo for
+for (let i = 0; i < students.length; i++) {
+  const student = students[i].id;
+  if (student === 2) {
+    console.log(students[i]);
+    break
+  }
+};
+
+// Recupera i dati dello studente con id 2 con forEach
+students.forEach((student, i) => {
+  if (students[i].id === 2) {
+    console.log(students[i]);
+  }
+});
+
+// Recupera i dati dello studente con id 2 con find
+const toFind = students.find((student, i) => {
+  return students[i].id === 2;
+});
+console.log(toFind);
+
+// Recupera i dati dello studente con id 2 con find one liner
+const toFindOl = students.find((student, i) => students[i].id === 2);
+console.log(toFindOl);
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
